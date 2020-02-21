47b4657b7a0555a3da887d06966469e1ca5e8c13
chongzixin <zixin@chope.co>
2020-02-21 22:58:05 +0800

    oop concepts, prototypes and inheritance

---
+++ section-12/oop.js
@@ -0,0 +1,40 @@
+function Dog(name, age) {
+    this.name = name;
+    this.age = age;
+    this.bark = function() {
+        console.log(this.name + " just barked!");
+    };
+}
+var doggie = new Dog("jack", 12);
+
+// EXERCISES
+// PART 1
+
+// Create a constructor function for a Person, each person should have a firstName, lastName, favoriteColor and favoriteNumber. Your function MUST be named Person. 
+
+// Write a method called multiplyFavoriteNumber that takes in a number and returns the product of the number and the object created from the Person functions' favorite number.
+
+function Person(firstName, lastName, favoriteColor, favoriteNumber) {
+    this.firstName = firstName;
+    this.lastName = lastName;
+    this.favoriteColor = favoriteColor;
+    this.favoriteNumber = favoriteNumber;
+    this.multipleFavoriteNumber = function (number) {
+        return number * this.favoriteNumber;
+    };
+}
+
+// PART 2
+
+// Given the following code - refactor the Child function to remove all the duplication from the Parent function. You should be able to remove 4 lines of code in the Child function and replace it with 1 single line.
+
+function Parent(firstName, lastName, favoriteColor, favoriteFood) {
+    this.firstName = firstName;
+    this.lastName = lastName;
+    this.favoriteColor = favoriteColor;
+    this.favoriteFood = favoriteFood;
+}
+
+function Child(firstName, lastName, favoriteColor, favoriteFood) {
+    Parent.apply(this, arguments);
+}
